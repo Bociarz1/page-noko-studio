8d4bdae46ef970e2ee58373559e08aa65617cdb6
Bociarz1 <no-email>
2026-05-26 16:00:27 +0200

    fix cloudflare imageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        imageService: "passthrough",
+    }),
     build: {
         inlineStylesheets: "always"
     },
